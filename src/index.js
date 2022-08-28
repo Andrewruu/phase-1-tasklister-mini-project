23f48dd211501e95f90293e69d1b39e5c22e582f
Andrew Wang <Andrewruujin@gmail.com>
2022-08-28 12:09:44 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    console.log(e.target.querySelector('#new-task-description').value)
+    toDo(e.target.querySelector('#new-task-description').value)
+  })
 });
+
+function toDo(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent =`${task} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
